2f62f31df6cab44e77f456834e57c7a3899f9f36
marc7666 <64266158+marc7666@users.noreply.github.com>
2022-05-16 19:52:31 +0200

    Headers modified

--- src/Pack.java
+++ src/Pack.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @project: untitled.
+ * @project: 3rd practical case Software engineering II.
  * @author: mcr99 on 16/05/2022.
  */
 public class Pack implements Product {
--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,5 @@
 /**
- * @project: untitled.
+ * @project: 3rd practical case Software engineering II.
  * @author: mcr99 on 16/05/2022.
  */
 public interface Product {
